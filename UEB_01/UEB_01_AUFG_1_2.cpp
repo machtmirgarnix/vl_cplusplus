a6a5266108cbf5685485f252f45854b143a162a7
machtmirgarnix <chiara.dietz@gmx.de>
2015-11-18 09:40:14 +0100

    Update UEB_01_AUFG_1_2.cpp

--- UEB_01/UEB_01_AUFG_1_2.cpp
+++ UEB_01/UEB_01_AUFG_1_2.cpp
@@ -4,6 +4,8 @@ int main(){
 
   int personen = 15;
   cout << "Es sind << personen <<  Personen anwesend !" << endl;
+  // Die Anführungszeichen fehlen
+  // cout << "Es sind" << personen <<  "Personen anwesend !" << endl;
 
   return 0;
 }
