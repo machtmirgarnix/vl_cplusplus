8dd42ed5f8f42f81c422fbe1052f1e99de5031fa
machtmirgarnix <chiara.dietz@gmx.de>
2015-11-18 09:41:18 +0100

    Update UEB_01_AUFG_1_3.cpp

--- UEB_01/UEB_01_AUFG_1_3.cpp
+++ UEB_01/UEB_01_AUFG_1_3.cpp
@@ -9,6 +9,7 @@ int main(){
     cout << "Der Raum ist zu voll!" << endl;
   } else (personen < 1)
       cout "Wo sind denn alle hin ??"
+  // Es sollte else if heißen, oder else ohne (personen < 1)
 
   return 0;
 }
